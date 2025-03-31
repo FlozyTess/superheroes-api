7dbb91ae4c726d59417048bc46f7728f7665e3ad
FlozyTess <cecilrency23@gmail.com>
2025-03-31 17:41:39 +0300

    definedthe power and hero power relationship

--- models.py
+++ models.py
@@ -25,3 +25,13 @@ class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
+
+    #relationship
+    hero_powers = db.relationship('HeroPower', backref='power', cascade="all, delete")
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
