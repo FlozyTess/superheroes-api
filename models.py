b65e41d5b01e17fdfb8160661a827463e580311f
FlozyTess <cecilrency23@gmail.com>
2025-03-31 17:27:16 +0300

    created class hero and the relationship with hero powers

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Hero(db.model):
+    __tablename__= 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    super_name = db.Column(db.String, nullable=False)
+  
+    #relationship
+    hero_powers = db.relationship('HeroPower', backref='hero', cascade="all, delete")
