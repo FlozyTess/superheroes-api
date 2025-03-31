b08d2c8b51b162fac1641699365eb85bbcfecaa9
FlozyTess <cecilrency23@gmail.com>
2025-03-31 16:13:29 +0300

    initialized the db

--- App.py
+++ App.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+app = Flask(__name__) #initialize app
+
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
