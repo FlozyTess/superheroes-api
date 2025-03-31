e837319809989d20eaffbe48c172f522248cd073
FlozyTess <cecilrency23@gmail.com>
2025-03-31 17:30:22 +0300

    defined heropower relationship

--- models.py
+++ models.py
@@ -9,5 +9,13 @@ class Hero(db.model):
     name = db.Column(db.String, nullable=False)
     super_name = db.Column(db.String, nullable=False)
   
-    #relationship
+    #relationship with hero power
     hero_powers = db.relationship('HeroPower', backref='hero', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "super_name": self.super_name,
+            "hero_powers": [hero_power.to_dict() for hero_power in self.hero_powers]
+        }
