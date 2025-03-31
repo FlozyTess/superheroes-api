34e5f7fe5d61f0dcc1cabd3ac75a55ccf7c160ce
FlozyTess <cecilrency23@gmail.com>
2025-03-31 15:24:49 +0300

    initialized the app

---
+++ app.py/models.py/hero model.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hero(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    super_name = db.Column(db.String, nullable=False)
+
+    hero_powers = db.relationship('HeroPower', backref='hero', cascade="all, delete")
+
